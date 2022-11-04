a267b6fbf7dbabca88b24c88284d9966da336e12
Hadi <hadifouani262@gmail.com>
2022-11-04 16:54:25 +0200

    class definition

--- classvector.cpp
+++ classvector.cpp
@@ -1,17 +1,30 @@
 #include <iostream>
 using namespace std;
-class Vector
+typedef int type;
+class MyVector
 {
     public:
-        
+        MyVector(int =1);
+        MyVector(type*, int );
+        MyVector(const MyVector&);
+        ~MyVector();
+        MyVector& operator=(const MyVector&);     
+        const type operator[] (int i) const;
+        type& operator[](int i);
+        MyVector& operator+= (const MyVector&);
 
+        void push_back(type);
+        type pop_back();
     private:
-
+        int size;
+        int capacity;
+        type* ptr;
+        friend ostream& operator<< (ostream&, const MyVector&);
 
 };
+const MyVector operator+ (MyVector v1, const MyVector& v2);
 int main()
 {
-
 
     return 0;
 }
